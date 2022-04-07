5738e73b94430e4e0822068277759070d35412d6
Nguyễn Nhật Minh <nnminh742@gmail.com>
2022-04-08 01:02:37 +0700

    update menu api

--- routes/s3.routes.js
+++ routes/s3.routes.js
@@ -3,7 +3,7 @@ const controller = require("../controllers/s3.controller");
 
 module.exports = function (app) {
     app.post("/api/s3/upload", [authJwt.verifyToken], controller.upload);
-    app.get("/api/s3/getListS3Objects", [authJwt.verifyToken], controller.getListS3Objects);
-    app.post("/api/s3/uploadS3Object", [authJwt.verifyToken], controller.uploadS3Object);
-    app.delete("/api/s3/deleteS3Object", [authJwt.verifyToken], controller.deleteS3Object);
+    app.get("/api/s3/getListS3Objects", [authJwt.verifyToken, authJwt.isAdmin], controller.getListS3Objects);
+    app.post("/api/s3/uploadS3Object", [authJwt.verifyToken, authJwt.isAdmin], controller.uploadS3Object);
+    app.delete("/api/s3/deleteS3Object", [authJwt.verifyToken, authJwt.isAdmin], controller.deleteS3Object);
 };
